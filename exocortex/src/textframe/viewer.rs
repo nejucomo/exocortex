fcb7bdf44c5e86648a9c28b5a11b806d97409200
nobody <nobody@nowhere.net>
2026-01-09 17:14:34 -0800

    Change the `Viewer` style to match the `Editor` more closely.

--- exocortex/src/textframe/viewer.rs
+++ exocortex/src/textframe/viewer.rs
@@ -1,5 +1,5 @@
 use derive_new::new;
-use eframe::egui::{Color32, Frame, Response, Stroke, Ui, Widget};
+use eframe::egui::{Frame, Response, Ui, Widget};
 use egui_commonmark::{CommonMarkCache, CommonMarkViewer};
 
 #[derive(new)]
@@ -11,9 +11,12 @@ pub(super) struct Viewer<'a> {
 impl<'a> Widget for Viewer<'a> {
     fn ui(self, ui: &mut Ui) -> Response {
         let avail = ui.available_size();
+        let visuals = ui.style().visuals.clone();
 
         Frame::NONE
-            .stroke(Stroke::new(1.0, Color32::RED))
+            .stroke(visuals.widgets.active.bg_stroke)
+            .fill(visuals.panel_fill)
+            .corner_radius(visuals.widgets.active.corner_radius)
             .show(ui, |ui| {
                 ui.set_min_size(avail);
                 CommonMarkViewer::new().show_mut(ui, self.cmcache, self.text);
