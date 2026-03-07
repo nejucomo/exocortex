4c9f2f9f9f882e9abf4828bd12d32bc47d2cfd34
nobody <nobody@nowhere.net>
2026-03-07 09:47:35 -0800

    Add `--db-*` arg parsing; not hooked in.

--- app/src/cliopts.rs
+++ app/src/cliopts.rs
@@ -1,7 +1,42 @@
-use clap::Parser;
+use std::path::{Path, PathBuf};
 
+use clap::{Args, Parser};
+
+/// `exocortex`: your handy attention and cognition toolkit
 #[derive(Parser)]
 pub struct Options {
     #[clap(flatten)]
     pub logopts: logging_options::StandardConsole,
+
+    #[clap(flatten)]
+    pub dbopts: DbOptions,
+}
+
+#[derive(Debug, Args)]
+#[group(multiple = false)]
+pub struct DbOptions {
+    /// Use ephemeral memory for the DB, aka no persistence
+    #[clap(long)]
+    db_mem: bool,
+
+    /// Use the given database path for persistence
+    #[clap(long)]
+    db_path: Option<PathBuf>,
+}
+
+impl DbOptions {
+    /// Get the database path; if `None` use memory only
+    pub fn path(&self) -> Option<&Path> {
+        if self.db_mem {
+            if self.db_path.is_none() {
+                None
+            } else {
+                unreachable!("clap arg parsing post-condition failure: {self:?}");
+            }
+        } else if let Some(p) = self.db_path.as_deref() {
+            Some(p)
+        } else {
+            todo!("default db path")
+        }
+    }
 }
