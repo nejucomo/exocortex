b5e92210fff4a290b1028bad844ce2ff1f31e5da
nobody <nobody@nowhere.net>
2026-03-12 18:25:31 -0700

    Define basic `...-reqrep` framework.

--- reqrep/src/lib.rs
+++ reqrep/src/lib.rs
@@ -1,14 +1,26 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+//! The `exocortex` <u>req</u>uest/<u>rep</u>ly nano-framework
+#![deny(missing_docs, unsafe_code)]
+
+/// A given [Request] is simply associated with a given [Reply](Request::Reply)
+pub trait Request {
+    /// The reply type for this type of request
+    type Reply;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+/// A base trait for [Queryable] / [Updatable] to define how those handlers may [Err]
+pub trait RequestError {
+    /// Errors [Self] may produce when queried or updated:
+    type Error: std::error::Error;
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Types which can be queried
+pub trait Queryable<Q: Request>: RequestError {
+    /// Query `self` with a `request`
+    fn query(&self, request: Q) -> Result<Q::Reply, Self::Error>;
+}
+
+/// Types which can be updated with a request to produce a response
+pub trait Updatable<U: Request>: RequestError {
+    /// Update `self` given `request`
+    fn update(&mut self, request: U) -> Result<U::Reply, Self::Error>;
 }
