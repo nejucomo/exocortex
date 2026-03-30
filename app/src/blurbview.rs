4cf5edb8e4d00942fa335d8303a28290265fe9ea
nobody <nobody@nowhere.net>
2026-03-30 15:56:19 -0600

    Replace the custom `BlurbView` innards with `ScrollArea`.

--- app/src/blurbview.rs
+++ app/src/blurbview.rs
@@ -1,5 +1,5 @@
 use derive_new::new;
-use eframe::egui::{Align, Layout, Response, Sense, Ui, Vec2};
+use eframe::egui::{Response, ScrollArea, Sense, Ui, Vec2};
 use egui_commonmark::CommonMarkCache;
 
 use crate::blurb::Blurb;
@@ -12,22 +12,16 @@ pub(crate) struct BlurbView<'a> {
 
 impl<'a> CommonMarkWidget for BlurbView<'a> {
     fn ui_with_cmcache(self, ui: &mut Ui, cmcache: &mut CommonMarkCache) -> Response {
-        let mut resp = ui.allocate_response(Vec2::ZERO, Sense::hover());
+        ScrollArea::vertical()
+            .show(ui, |ui| {
+                let mut r = ui.allocate_response(Vec2::ZERO, Sense::hover());
 
-        ui.with_layout(Layout::top_down(Align::Min), |ui| {
-            ui.set_max_width(ui.available_width());
-
-            let bottom = ui.clip_rect().bottom();
-
-            for blurb in self.blurbs.iter().rev() {
-                resp |= blurb.ui_with_cmcache(ui, cmcache);
-
-                if resp.rect.bottom() > bottom {
-                    break;
+                for blurb in self.blurbs.iter().rev() {
+                    r |= blurb.ui_with_cmcache(ui, cmcache);
                 }
-            }
-        });
 
-        resp
+                r
+            })
+            .inner
     }
 }
