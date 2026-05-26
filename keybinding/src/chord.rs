02486f7124442734000abacf1b37adb3ab3a0272
nobody <nobody@nowhere.net>
2026-05-26 07:57:56 -0700

    Tweak the shortcut help display to be pretty.

--- keybinding/src/chord.rs
+++ keybinding/src/chord.rs
@@ -1,4 +1,5 @@
-use egui::{Key, ModifierNames, Modifiers, Response, RichText, Ui, Widget};
+use egui::{Color32, Frame, Key, Margin, ModifierNames, Modifiers, Response, RichText, Ui, Widget};
+use tap::Tap as _;
 
 /// Equivalent to [egui::KeyboardShortcut] with additional [From]/[Into] impls
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Hash)]
@@ -30,13 +31,27 @@ impl From<(Key, Modifiers)> for KeyChord {
 
 impl Widget for KeyChord {
     fn ui(self, ui: &mut Ui) -> Response {
-        ui.label(
-            RichText::new(format!(
-                "{} {}",
-                ModifierNames::SYMBOLS.format(&self.modifiers, ui.ctx().os().is_mac()),
-                self.key.symbol_or_name()
-            ))
-            .code(),
-        )
+        let crad: f32 = ui.text_style_height(&egui::TextStyle::Body) / 2.0;
+
+        Frame::group(ui.style())
+            .tap_mut(|f| {
+                let c = Color32::BLUE;
+                f.fill = f.fill.lerp_to_gamma(c, 0.1);
+                f.stroke.color = f.stroke.color.lerp_to_gamma(c, 0.2);
+                f.stroke.width *= 0.5;
+            })
+            .corner_radius(crad)
+            .inner_margin(Margin::same((crad / 2.0) as i8))
+            .show(ui, |ui| {
+                ui.label(
+                    RichText::new(format!(
+                        "{} {}",
+                        ModifierNames::SYMBOLS.format(&self.modifiers, ui.ctx().os().is_mac()),
+                        self.key.symbol_or_name()
+                    ))
+                    .monospace(),
+                )
+            })
+            .response
     }
 }
